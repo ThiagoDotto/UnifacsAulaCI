51a3e119092aed6e0c5f4acaf1c95f0ae8776103
thiago <tdotto@gmail.com>
2024-04-18 08:12:26 -0300

    teste de CI

--- src/main/java/br/com/aulaunifacs/AulaCI.java
+++ src/main/java/br/com/aulaunifacs/AulaCI.java
@@ -1,4 +1,17 @@
 package br.com.aulaunifacs;
 
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Repository
 public class AulaCI {
+
+
+    @GetMapping("/")
+    public String testeCI(){
+        return "CI Funcionando";
+    }
+
+
 }
